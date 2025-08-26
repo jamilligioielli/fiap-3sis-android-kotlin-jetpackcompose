cba18d47c6fed027e23450ea6c537141fe22973d
jamilligioielli <jamilligioielli@outlook.com>
2025-08-26 20:51:00 -0300

    layout card mensagem aula 21-08

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Fiap-3sis-android-kotlin-jetpackcompose"
+include(":app")
